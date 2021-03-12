96ec11b164d09b16d0a2d9d54114af24562be641
Grensot <67422721+Grensot@users.noreply.github.com>
2021-03-12 20:05:07 +0300

    Add files via upload

---
+++ Assets/Scripts/Open_Close.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open_Close : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Doro;
+    private Animator doorAnim;
+    private float distance;
+    public float interdist = 2f;
+    public KeyCode Open_close = KeyCode.E;
+    private bool doorOpen = false;
+    private void Awake()
+    {
+        doorAnim = Doro.GetComponent<Animator>();
+    }
+    void OnMouseOver()
+    {
+        distance = Vector3.Distance(Player.GetComponent<Transform>().position, transform.position);
+        if (distance < interdist)
+        {
+
+            if (Input.GetKeyDown(Open_close))
+            {
+
+                if (!doorOpen)
+                {
+                    doorAnim.Play("DoorO", 0, 0.0f);
+                    doorOpen = true;
+                }
+                else
+                {
+                    doorAnim.Play("DoorC", 0, 0.0f);
+                    doorOpen = false;
+                }
+            }
+        }
+    }
+}
