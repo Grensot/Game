37b585aa5349d84d73973149b3bf7de95b56e810
Grensot <67422721+Grensot@users.noreply.github.com>
2021-03-12 11:02:23 +0300

    Add files via upload

---
+++ Scripts/Open_Close.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open_Close : MonoBehaviour
+{
+    public GameObject Player;
+    private DoorAnimator Door;
+    private float distance;
+    public float interdist = 2f;
+    public KeyCode Open_close = KeyCode.E;
+
+
+    void OnMouseOver()
+    {
+        distance = Vector3.Distance(Player.GetComponent<Transform>().position, transform.position);
+        if (distance < interdist)
+        {
+            Debug.Log("Все норм");
+            if (Input.GetKeyDown(Open_close))
+            {
+                Door.PlayAnimation();
+            }
+        }
+    }
+}
---
+++ Scripts/Take_Drop.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Take_Drop : MonoBehaviour
+{
+    public GameObject Player;
+    private float distance;
+    public float interdist = 2f;
+    public Transform arm;
+    public KeyCode TakeObject = KeyCode.E;
+    public KeyCode RemoveObject = KeyCode.Q;
+
+    void OnMouseOver()
+    {
+        distance = Vector3.Distance(Player.GetComponent<Transform>().position, transform.position);
+        if(distance < interdist)
+        {
+            if(Input.GetKeyDown (TakeObject))
+            {
+                GetComponent<Rigidbody>().isKinematic = true;
+                transform.position = arm.position;
+                transform.rotation = arm.rotation;
+                transform.SetParent(arm);
+            }
+        }
+    }
+    void Update ()
+    {
+        if (Input.GetKeyDown(RemoveObject))
+        {
+            GetComponent<Rigidbody>().isKinematic = false;
+            transform.parent = null;
+        }
+    }
+
+
+}
