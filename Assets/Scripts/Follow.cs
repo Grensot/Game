2dbe959a899c210f822548ec05f8678fea8f12e1
Grensot <67422721+Grensot@users.noreply.github.com>
2021-03-19 16:42:20 +0300

    Add files via upload

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public UnityEngine.AI.NavMeshAgent enemy;
+    public Transform Object;
+    public Transform Tele2;
+    void FixedUpdate()
+    {
+        enemy.SetDestination(Object.position);
+    }
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        if (collisionInfo.collider.name == "Player")
+        {
+            Object.transform.position = Tele2.position;
+            Destroy(gameObject);
+        }
+    }
+}
