0878a452d60baf055a9b263222bb94305ee1f2f7
Grensot <67422721+Grensot@users.noreply.github.com>
2021-03-23 17:13:19 +0300

    Add files via upload

---
+++ Assets/Scripts/DoorAnimator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimator : MonoBehaviour
+{
+
+    private Animator doorAnim;
+    
+
+    private bool doorOpen = false;
+    private void Awake()
+    {
+        doorAnim = gameObject.GetComponent<Animator>();
+    }
+
+
+    public void PlayAnimation()
+    {
+        if (!doorOpen)
+        {
+            doorAnim.Play("DoorO", 0, 0.0f);
+            doorOpen = true;
+        }
+        else
+        {
+            doorAnim.Play("DoorC", 0, 0.0f);
+            doorOpen = false;
+        }
+    }
+}
---
+++ Assets/Scripts/TeleDisap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleDisap : MonoBehaviour
+{
+    public GameObject Obj;
+    public GameObject Player;
+    void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag == "ground")
+        {
+            Player.transform.position = Obj.transform.position;
+            Destroy(Obj);
+        }
+    }
+}
