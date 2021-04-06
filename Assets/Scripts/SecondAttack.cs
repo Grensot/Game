60e9e70caa1a15e7a0ac5a6260b760adc51408ad
Grensot <67422721+Grensot@users.noreply.github.com>
2021-04-06 19:03:16 +0300

    Add files via upload

---
+++ Assets/Scripts/SecondAttack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondAttack : MonoBehaviour
+{
+    public GameObject Player;
+    public float speed = 10.0f;
+    private float currentTime;
+    private Rigidbody InsObj;
+    public Rigidbody[] CopyObject;
+
+    void FixedUpdate()
+    {
+        float step = speed * Time.deltaTime;
+        currentTime += Time.deltaTime;
+        if(currentTime <= 10)
+        {
+            transform.LookAt(Player.transform.position);
+            if (currentTime % 2 == 0)
+            {
+                InsObj = Instantiate(CopyObject[0], transform.position, transform.rotation);
+            }
+        }
+        InsObj.velocity = transform.TransformDirection(Vector3.down * 10);
+    }
+}
