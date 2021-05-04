45567d1dc338bbb1b317c6c4191deba62930b837
Grensot <67422721+Grensot@users.noreply.github.com>
2021-05-04 14:10:04 +0300

    Add files via upload

---
+++ Scripts/EnemyAi.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAi : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject[] CopyObject;
+    private GameObject InsObj;
+    public GameObject Enemy;
+    private Transform Point;
+    public Transform player;
+    private bool ObjectCreated;
+
+
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    public float sightRange;
+    public bool playerInSightRange;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        ObjectCreated = false;
+    }
+
+    private void Update()
+    {
+
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        if (playerInSightRange) ChasePlayer();
+    }
+    private void ChasePlayer()
+    {
+        Enemy.GetComponent<WaypointPatrol>().enabled = false;
+        if (!ObjectCreated)
+        {
+            ObjectCreated = true;
+            InsObj = Instantiate(CopyObject[0]);
+            InsObj.transform.position = player.position;
+            InsObj.SetActive(true);
+            Point = InsObj.GetComponent<Transform>();
+        }
+        agent.SetDestination(Point.position);
+    }
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider == InsObj)
+        {
+            Enemy.GetComponent<WaypointPatrol>().enabled = true;
+            Destroy(InsObj);
+            ObjectCreated = false;
+        }
+    }
+}
---
+++ Scripts/Instance.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instance : MonoBehaviour
+{
+    public GameObject[] CopyObject;
+    public GameObject InsObj;
+    public Transform Position;
+    private bool isObjCreated;
+    void Start ()
+    {
+        isObjCreated = false;
+    }
+    void OnTriggerEnter(Collider Player)
+    {
+        if (!isObjCreated)
+        {
+            InsObj = Instantiate(CopyObject[0]);
+            InsObj.transform.position = Position.transform.position;
+            InsObj.SetActive(true);
+            isObjCreated = true;
+        }
+    }
+    
+}
