4aa6bf7807f615ac16d92a4226cd993109b0127b
Grensot <67422721+Grensot@users.noreply.github.com>
2021-05-05 23:31:34 +0300

    Add files via upload

---
+++ Assets/Scripts/ArenaInvoke.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaInvoke : MonoBehaviour
+{
+    public float currentTime;
+    public GameObject[] CopyObject;
+    public GameObject InsObj;
+    public Transform Position;
+
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void FixedUpdate()
+    {
+        currentTime += Time.deltaTime;
+        if(currentTime == 5)
+        {
+            InsObj = Instantiate(CopyObject[0]);
+            InsObj.transform.position = Position.transform.position;
+            InsObj.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Movement : MonoBehaviour
+{
+    public GameObject costil;
+
+    float Ver, Hor, Jump;
+
+    public bool isGround;
+
+    public float Speed = 10, JumpSpeed = 200;
+
+
+    void FixedUpdate()
+    {
+
+        transform.rotation = costil.transform.rotation;
+
+        if(isGround)
+        {
+            Ver = Input.GetAxis("Vertical") * Time.deltaTime * Speed;
+            Hor = Input.GetAxis("Horizontal") * Time.deltaTime * Speed;
+            Jump = Input.GetAxis("Jump") * Time.deltaTime * JumpSpeed;
+            GetComponent<Rigidbody>().AddForce(transform.up * Jump, ForceMode.Impulse);
+        }
+        transform.Translate(new Vector3(Hor, 0, Ver));
+    }
+}
