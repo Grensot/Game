3d652c3e38217c6d06b742ae1c57b856b19e1e12
Grensot <67422721+Grensot@users.noreply.github.com>
2021-09-28 14:49:44 +0300

    Add files via upload

---
+++ Assets/Scripts/Factories/GenericFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = System.Object;
+
+public class SpawnFactory<T> : MonoBehaviour where T : Transform
+{
+    [SerializeField] private T _prefab;
+    [SerializeField] private Transform _pointToSpawn;
+    private int n = 0;
+
+    public T GetNewInstance()
+    {
+        Vector3 pos = new Vector3(_pointToSpawn.position.x, _prefab.transform.position.y, _pointToSpawn.position.z+n);
+        n++;
+
+        return Instantiate(_prefab, pos, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Factories/PressF.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressF : MonoBehaviour
+{
+    [SerializeField] private TrapFactory _trapFactory;
+
+    void FixedUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            TrapKey();
+            return;
+        }
+
+    }
+    public void TrapKey()
+    {
+     var prefab = _trapFactory.GetNewInstance();
+    }
+}
